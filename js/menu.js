0e6778adc183f84eb2c2a254e0b92c121bab54fc
Sofia Acevedo <sofiacandelaria1992@gmail.com>
2023-11-02 17:31:08 +0000

    enlazar menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', (event)=>{
-    let button = document.querySelector(".layout__menu-toggle")
+    let button = document.querySelector(".layout__menu-toggle");
+    let aside= document.querySelector(".header");
+
+    button.addEventListener('click', (event)=>{
+        let visible = document.querySelector(".header--visible");
+
+        if (visible){
+            aside.classList.add("header--visible");
+        }else{
+            aside.classList.remove("header--visible");
+        }
+    })
 })
